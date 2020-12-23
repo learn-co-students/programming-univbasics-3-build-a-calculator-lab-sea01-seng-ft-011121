6e1a6816d5ae216928156af7844970312c69384f
Colin Schlecht <schlechtcolin@yahoo.com>
2020-12-22 22:46:36 -0800

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+    ans = num1 + num2
+    return ans
 end
 
 def subtraction(num1, num2)
-
+    ans = num1 - num2
+    return ans
 end
 
 def division(num1, num2)
-
+    ans = num1 / num2
+    return ans
 end
 
 def multiplication(num1, num2)
-
+    ans = num1 * num2
+    return ans
 end
 
 def modulo(num1, num2)
-
+    ans = num1 % num2
+    return ans
 end
 
 def square_root(num)
-
+    ans = Math.sqrt(num)
+    return ans
 end
